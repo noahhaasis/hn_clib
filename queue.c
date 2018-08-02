248eac7c788249cd0e73c509db2a2e39af42b251
noahhaasis <haasis_noah@yahoo.de>
2018-08-02 05:43:42 +0200

    Add a queue data structure

---
+++ queue.c
@@ -0,0 +1,38 @@
+#include "queue.h"
+
+#include <stdlib.h>
+
+
+void queue_create(queue_t *queue) {
+    return (queue_t *)calloc(sizeof(queue_t), 1);
+}
+
+void enqueue(void *elem, queue_t *queue) {
+    queue_elem_t *new_elem = (queue_elem_t *)malloc(sizeof(queue_elem_t));
+    new_elem->next = queue->start;
+    queue->start = new_elem;
+}
+
+void *dequeue(queue_t *queue) {
+    queue_t *res = queue->start;
+    if (!res) { return NULL; };
+    queue->start = res->next;
+    return res;
+}
+
+void queue_destroy(queue_t *queue) {
+    queue_elem_t *next;
+    queue_elem_t *elem = queue->start;
+    while (elem) {
+        next = elem->next;
+        free(elem);
+        elem = next;
+    }
+}
+
+int queue_len(queue_t *queue) {
+    if (!queue) {
+        return 0;
+    }
+    return queue->length;
+}
---
+++ queue.h
@@ -0,0 +1,30 @@
+#ifndef QUEUE_H
+#define QUEUE_H
+
+typedef struct queue_elem_t {
+    void *data;
+    struct queue_elem_t *next;
+} queue_elem_t;
+
+typedef struct queue_t {
+    queue_elem_t *start;
+    queue_elem_t *end;
+    int length;
+} queue_t;
+
+/* Creates a empty queue at the passed location */
+void queue_create(queue_t *queue);
+
+/* Adds an element at the end of the queue */
+void enqueue(void *elem, queue_t *queue);
+
+/* Returns the element at the queues front (FIFO) */
+void *dequeue(queue_t *queue);
+
+/* Removes all elements from the queue 
+ * but doesn't affect the queue_t or the elements itself. */
+void queue_destroy_remove_elements(queue_t *queue);
+
+int queue_len(queue_t *queue);
+
+#endif
