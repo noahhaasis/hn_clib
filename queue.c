b7d9345ff5b4134a8731b5fe24096a4719c071ec
noahhaasis <haasis_noah@yahoo.de>
2018-08-02 06:25:39 +0200

    Increase and decrease at the right locations

--- queue.c
+++ queue.c
@@ -13,7 +13,7 @@ void enqueue(void *elem, queue_t *queue) {
     queue_elem_t *new_elem = (queue_elem_t *)malloc(sizeof(queue_elem_t));
     new_elem->next = queue->start;
     queue->start = new_elem;
-    queue->length -= 1;
+    queue->length += 1;
 }
 
 void *dequeue(queue_t *queue) {
@@ -21,7 +21,7 @@ void *dequeue(queue_t *queue) {
     queue_elem_t *res = queue->start;
     if (!res) { return NULL; };
     queue->start = res->next;
-    queue->length += 1;
+    queue->length -= 1;
     return res->data;
 }
 
