aef03be7da37c77ebf60d91b50e12ad969dacd77
Noah Haasis <haasis_noah@yahoo.de>
2018-09-14 07:35:57 +0200

    Add a warning

--- array.c
+++ array.c
@@ -1,6 +1,6 @@
 #include "array.h"
 
-
+// WARNING: This only does a pointer comparison
 bool array_contains(void *arr[], int arr_len, void *elem) {
     for (int i = 0; i < arr_len; i++) {
         if (arr[i] == elem) { return true; }
